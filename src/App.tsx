6ef048c44c48728d40e4d2420f91630f64bccd04
Mackhaster <mrshanelyse@hotmail.com>
2022-11-03 09:22:16 -0700

    Add cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import { Container } from 'react-bootstrap'
 import { Home } from './pages/home'
 import { About } from './pages/About'
 import { Store } from './pages/Store'
+import { Navbar } from './components/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
 
 
 function App() {
-  return <Container className='mb-4'>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/store' element={<Store />} />
-      <Route path='/about' element={<About />} />
-    </Routes>
-  </Container>
+  return (
+  <ShoppingCartProvider>
+    <Navbar />
+    <Container className='mb-4'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+  </ShoppingCartProvider>
+  )
 }
 
 export default App
